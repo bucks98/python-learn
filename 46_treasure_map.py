00ad2a41b8700378dd1754ba06e97e1fd78ea5bd
Bagavath <bagavathprofile@gmail.com>
2022-10-06 12:27:23 +0530

    replaced emoji with o

--- 46_treasure_map.py
+++ 46_treasure_map.py
@@ -1,7 +1,7 @@
 # 🚨 Don't change the code below 👇
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️️","⬜️️","⬜️️"]
+row1 = ["0","️0","️0"]
+row2 = ["0","0","️0"]
+row3 = ["0","0","0"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
