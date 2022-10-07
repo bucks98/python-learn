012b3289bbdd2d5e51db5db75f319352fd6cc8d6
Bagavath <bagavathprofile@gmail.com>
2022-10-07 18:03:15 +0530

    fizzbuzz exercise

---
+++ fizzbuzz_exercise.py
@@ -0,0 +1,16 @@
+#Write your code below this row 👇
+for three in range (1, 101):
+  if three % 3 == 0 and three % 5 == 0:
+    print ("fizzbuzz")
+  elif three % 3 == 0:
+    print ("fizz")
+  elif three % 5 == 0:
+    print ("buzz")
+  else:
+    print (three)
+    
+    
+        
+
+  
+    
